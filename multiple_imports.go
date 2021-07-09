0b4e8333f2c252e909fc0bd83adf6889220d1393
rishavghosh605 <rishavghosh605@gmail.com>
2021-07-09 20:15:12 +0530

    First commit

---
+++ multiple_imports.go
@@ -0,0 +1,9 @@
+package main
+
+import ("fmt",
+		"match")
+
+//Functions in the package all start with a capital letter
+func main (){
+	fmt.Println("The square root of 4 is",math.Sqrt(4));
+}
---
+++ tutorial.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"//short form for format
+
+func main() {
+	fmt.Println("Welcome to go")
+}
