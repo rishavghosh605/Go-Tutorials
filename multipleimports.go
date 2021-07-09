cf8ddb232cb1b8d5e87e85559d1b46f0a027095d
rishavghosh605 <rishavghosh605@gmail.com>
2021-07-09 20:30:47 +0530

    Modified file name

--- multipleimports.go
+++ multipleimports.go
@@ -1,9 +1,9 @@
 package main
 
-import ("fmt",
-		"match")
+import ("fmt"
+		"math")
 
 //Functions in the package all start with a capital letter
 func main (){
-	fmt.Println("The square root of 4 is",math.Sqrt(4));
+	fmt.Println("The square root of 4 is",math.Sqrt(4))
 }
